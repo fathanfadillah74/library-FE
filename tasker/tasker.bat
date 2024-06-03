19aee404d1588b12ed912005e37ea6e38163f3b9
fathanfadillah74 <fathanfadillah74@gmail.com>
2024-06-03 21:12:37 +0700

    update

--- tasker/tasker.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Tasker script for copying the config folder and its contents
-
-echo Copying config folder and its contents...
-
-xcopy config %~dp0\config /E /I /Y
-
-echo Copy complete.
-pause
